8204a4138e7486509cc0cd011c773b5f44a4cc94
Amit Kumar Das <amit.inverse@gmail.com>
2024-02-14 08:49:45 +0530

    004

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controllers
+  TextEditingController nameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+
+  // open new expense box
+  void openNewExpenseBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('New expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // user input -> expense name
+            TextField(
+              controller: nameController,
+            ),
+
+            // user input -> expense amount
+            TextField(
+              controller: amountController,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNewExpenseBox,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
