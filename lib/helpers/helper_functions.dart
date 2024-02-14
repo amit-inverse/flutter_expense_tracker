8d04707b38173b01e738b1fb0606b3fa8da915a5
Amit Kumar Das <amit.inverse@gmail.com>
2024-02-14 22:42:28 +0530

    007

--- lib/helpers/helper_functions.dart
+++ lib/helpers/helper_functions.dart
@@ -4,9 +4,22 @@ These are some helpful functions used accross the app
 
 */
 
+import 'package:intl/intl.dart';
+
 // convert string to a double
 double convertStringToDouble(String string) {
   double? amount = double.tryParse(string);
 
   return amount ?? 0;
+}
+
+// format double amount into rupee & paisa
+String formatAmount(double amount) {
+  final format = NumberFormat.currency(
+    locale: "en_US",
+    symbol: "₹",
+    decimalDigits: 2,
+  );
+
+  return format.format(amount);
 }
